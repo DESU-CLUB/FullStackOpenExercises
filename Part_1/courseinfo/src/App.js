77d5eefcbc2fba1bcb5e81e4cbcebe5b53747139
DESU-CLUB <warrenlowkh@gmail.com>
2022-12-30 00:20:09 +0800

    Did Exercises 1.3-1.5

--- Part_1/courseinfo/src/App.js
+++ Part_1/courseinfo/src/App.js
@@ -1,36 +1,49 @@
 
 
 function App() {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  
+  
   return (
     <div className="App">
       <Header course = {course}/>
-      <Content part1 = {part1} exercises1 = {exercises1} 
-      part2 = {part2} exercises2 = {exercises2} 
-      part3 = {part3} exercises3 = {exercises3}/>
-      <Total number = {exercises1 + exercises2 + exercises3}/>
+      <Content parts = {course}/>
+      <Total parts = {course}/>
     </div>
   );
 }
 
 function Header(props){
   return(
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   );
 }
 
 function Content(props){
+  let arr = []
+  let idx = 1
+  props.parts.parts.forEach(value => {arr = arr.concat(<Part key = {idx++} part = {value.name} exercise = {value.exercises} />)})
+
   return(
     <div>
-      <Part part = {props.part1} exercise = {props.exercises1}/>
-      <Part part = {props.part2} exercise = {props.exercises2}/>
-      <Part part = {props.part3} exercise = {props.exercises3}/>
+      {arr}
     </div>
 
   );
@@ -43,8 +56,15 @@ function Part(props){
 }
 
 function Total(props){
+  function getSum(arr){
+    let sum = 0
+    arr.forEach(value => sum+=value.exercises)
+    return sum
+
+  }
+  const total = getSum(props.parts.parts)
   return(
-    <p>{props.number}</p>
+    <p>{total}</p>
   );
 }
 
